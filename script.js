796f7b2b19889faf0fcdf064e0bbcf5cf2259b91
donyaesolo <35720283+donyaesolo@users.noreply.github.com>
2019-02-07 14:45:42 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-var side = 0;
+var i = 0;
 
-function left(el) {
-	if (side == 0){
-		document.body.style.backgroundImage = "url('wall.png')";
-		document.getElementById("pat").classList.add("blue");
+function base() {
+	document.getElementById("change").style.backgroundImage = "url('images/wall.jpg')";
+}
+function left() {
+	if (i == 0){
+		  document.getElementById("change").style.backgroundImage = "url('images/wall.jpg')";
+		  document.images["rock"].src ="";
+		  document.images["door"].src ="images/door.png";
+		  i = i + 1;
+	} else if (i == 2) {
+			  i = i - 2;
 	} else {
-	
+		document.getElementById("change").style.backgroundImage = "url('images/wallb.png')";
+		document.images["door"].src = "images/paper.png";
+		document.images["paper"].src ="";
+	}
+}
+
+function right() {
+	if (i == 1){
+		 	i =  i - 1;
+	} else if (i == 0) {
+			document.getElementById("change").style.backgroundImage = "url('images/wall.jpg')";
+			document.images["paper"].src ="";
+			 document.images["door"].src ="images/door.png";
+			i =  i + 2;
+	} else{
+		document.getElementById("change").style.backgroundImage = "url('images/wallb.png')";
+		document.images["rock"].src = "images/rock.png";
+		document.images["door"].src ="";
 	}
 }
