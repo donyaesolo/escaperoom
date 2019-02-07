1d508deb668be1aa99b8fad5bf7bfbabe63cef53
donyaesolo <35720283+donyaesolo@users.noreply.github.com>
2019-02-07 13:15:47 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var side = 0;
+
+function left(el) {
+	if (side == 0){
+		document.body.style.backgroundImage = "url('wall.png')";
+		document.getElementById("pat").classList.add("blue");
+	} else {
+	
+	}
+}
